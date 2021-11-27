c712106e16f88f7246a366a3bdf8796bf6c4a366
quyenkaneki2k7 <95002837+quyenkaneki2k7@users.noreply.github.com>
2021-11-27 19:53:52 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.rgb(127,255,0)('[ MY NAME IS THANHF ] » ') + data);
+			console.log(chalk.rgb(127,255,0)('[ ★Ҩųƴềทℳƴ★ ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.rgb(255,20,147)('[ MY NAME IS THANHF ] » ') + data);
+			console.log(chalk.rgb(255,20,147)('[ ★Ҩųƴềทℳƴ★ ] » ') + data);
 			break;
 		default:
 			console.log(chalk.rgb(230,230,230)(`${option} » `) + data);
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.rgb(0,206,209)('[ TUI LÀ THÀNH ] » ') + data);
+			console.log(chalk.rgb(0,206,209)('[ ★Ҩųƴềทℳƴ★ ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.rgb(255,105,180)('[ TUI LÀ THÀNH ] » ') + data);
+			console.log(chalk.rgb(255,105,180)('[ ★Ҩųƴềทℳƴ★ ] » ') + data);
 			break;
 		default:
-			console.log(chalk.rgb(64,224,208)(`[ TUI LÀ THÀNH ] » `) + data);
+			console.log(chalk.rgb(64,224,208)(`[ ★Ҩųƴềทℳƴ★ ] » `) + data);
 			break;
 	}
 }
